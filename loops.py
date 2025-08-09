0936da1aca9a245f6fe905e58c191373d73445b8
axamed <cumarmaxamed936@gmail.com>
2025-08-09 12:19:58 +0300

    first project  test

---
+++ loops.py
@@ -0,0 +1,10 @@
+magac="mohamed omar mohamed"
+name=""
+for mag in magac:
+    if mag!=" ":
+        magac+=mag
+    else:
+        print(f"magaca waa:{name}")
+        name=""
+    print(f"magaca waa:{name}")
+        
