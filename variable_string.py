149e13fdea8c381d221f615389fdda862bb02bd6
axamed <cumarmaxamed936@gmail.com>
2025-08-09 13:57:19 +0300

    adding

---
+++ variable_string.py
@@ -0,0 +1,16 @@
+# # msg='hello word'
+# # print(msg)
+
+
+# # combining strings
+
+# first_name='mohamed '
+# middle_name=' omar '
+# last_name=' mohamed '
+# full_name= first_name+middle_name+last_name 
+# print(full_name)
+
+full_name="mohamed omar mohamed\n "
+school='haniwadaag\n'
+age=(122)
+print("myname is ",full_name,"myschool",school,"my age",age)
